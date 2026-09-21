e0fe0a2d07a08052c402234dc0294a29392b4462
fernanda130909 <fernanda.kraicz@escola.pr.gov.br>
2026-09-21 08:22:31 -0300

    oi

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    // Evita que a página recarregue ao enviar o formulário
+    
     event.preventDefault(); 
 
     const nome = document.getElementById('nome').value;
     const senha = document.getElementById('senha').value;
     const mensagemErro = document.getElementById('mensagemErro');
 
-    // Verifica se os dados estão corretos
+    
     if (nome === "admin" && senha === "1234") {
-        // Limpa a mensagem de erro, caso exista
+        
         mensagemErro.textContent = "";
         
-        // Redireciona o usuário para outro link
+        
         window.location.href = "https://example.com"; 
     } else {
         mensagemErro.textContent = "Nome ou senha incorretos!";
